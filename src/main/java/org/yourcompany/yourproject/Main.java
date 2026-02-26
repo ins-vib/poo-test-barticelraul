9474d27c8a3a3886bac30f68544018a2e5a1b0ec
Raul Barticel <raba122@vidalibarraquer.net>
2026-02-26 13:21:54 +0100

    Final corregit

--- src/main/java/org/yourcompany/yourproject/Main.java
+++ src/main/java/org/yourcompany/yourproject/Main.java
@@ -50,12 +50,12 @@ public class Main {
         ArrayList<Pregunta> preguntes = new ArrayList<>();
         preguntes.add(new Pregunta("Qui va pintar el Guernica", new String[]{"Velàzquez", "Goya", "Picasso"}, 2));
         preguntes.add(new Pregunta("Qui va escriure Alicia al país de les meravelles", new String[]{"Grimm", "Stevenson", "Carrol"}, 2));
-        preguntes.add(new Pregunta("Quina és la magnitud que relaciona espai i temps", new String[]{"velocitat", "temperatura", "pes"}, 0));
+        preguntes.add(new Pregunta("Quina és la magnitud que relaciona espai i temps", new String[]{"velocitat", "temperatura", "pes"}, 1));
         preguntes.add(new Pregunta("Si el radi d'una circumferència és 4, el seu diàmetre és", new String[]{"4", "8", "12", "14"}, 1));
         preguntes.add(new Pregunta("Quants segons té 1 hora", new String[]{"420", "760", "3600"}, 2));
         preguntes.add(new Pregunta("Quin no és un llenguatge de programació", new String[]{"java", "php", "samsung"}, 2));
         preguntes.add(new Pregunta("Dins un termòmetre hi trobem", new String[]{"aigua", "aigua amb gas", "mercuri"}, 2));
-        preguntes.add(new Pregunta("L'Acropolis es troba a", new String[]{"Atenes", "Roma", "París"}, 0));
+        preguntes.add(new Pregunta("L'Acropolis es troba a", new String[]{"Atenes", "Roma", "París"}, 2));
         preguntes.add(new Pregunta("L'element químic amb símbol Fe és ", new String[]{"Estronci", "Ferro", "Feril·li"}, 1));
         preguntes.add(new Pregunta("La capital d'Estats Units és", new String[]{"Georgetown", "New York", "Washington"}, 2));
         return preguntes;
